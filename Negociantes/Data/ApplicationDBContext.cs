3b957a99f3a5ccb462921d06c6d4e45a4510ec6c
dldygnl <53053198+itsjeffersonli@users.noreply.github.com>
2021-03-10 15:54:46 +0800

    added Negociantes, but cannot build

---
+++ Negociantes/Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Negociantes.Models;
+
+namespace Negociantes.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Users> User { get; set; }
+    }
+}
